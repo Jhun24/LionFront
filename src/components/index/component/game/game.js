b73ed1f97bf198361e525a2ba762c467abde6e0e
Jhun24 <jjanghuni102@naver.com>
2020-10-25 02:05:00 +0900

    game fix

--- src/components/index/component/game/game.js
+++ src/components/index/component/game/game.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import Content from './content/content'
+
 import './game.css'
 
 class Game extends Component{
@@ -12,7 +14,7 @@ class Game extends Component{
                 <h2>재활용 게임</h2>
                 <p>무덤 별 계절이 무성할 봅니다. 그러나 강아지, 차 이런 헤일 나의 딴은 언덕 까닭입니다. 지나고 못 이국 이런 당신은 하나에 라이너 거외다. 노새, 하나에 이국 남은 불러 헤는 거외다. </p>
                 <div className="game-content">
-
+                    <Content></Content>
                 </div>
             </div>
         );
