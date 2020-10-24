592a1fdd713149a8adad44a7dc3a064c9d00e00b
Jhun24 <jjanghuni102@naver.com>
2020-10-25 05:55:36 +0900

    fix

--- src/components/index/component/game/game.js
+++ src/components/index/component/game/game.js
@@ -24,7 +24,7 @@ class Game extends Component{
                 <h2>재활용 게임</h2>
                 <p>무덤 별 계절이 무성할 봅니다. 그러나 강아지, 차 이런 헤일 나의 딴은 언덕 까닭입니다. 지나고 못 이국 이런 당신은 하나에 라이너 거외다. 노새, 하나에 이국 남은 불러 헤는 거외다. </p>
                 <div className="game-content">
-                    {Button}
+                    {gameDisplay}
                 </div>
             </div>
         );
